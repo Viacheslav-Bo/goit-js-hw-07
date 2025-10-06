39f5e89caecbdd1633d9184549b19ce42412bbc9
Viacheslav-Bo-PC <zotikivna@gmail.com>
2025-10-07 01:29:27 +0300

    some work

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
+// елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+
+// const listCategories = document.querySelectorAll("#categories li.item");
+// console.log(`Number of categories: ${listCategories.length}`);
+
+// listCategories.forEach((item) => {
+//   const categoryName = item.querySelector("h2").textContent;
+//   const itemCount = item.querySelectorAll("li").length;
+
+//   console.log(`Category: ${categoryName}`);
+//   console.log(`Elements: ${itemCount}`);
+// });
